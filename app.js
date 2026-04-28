08a8999dc87fe566288551a8d4dd23d6c8ee41f0
unknown <youstinaatef748@gmail.com>
2026-04-28 20:06:43 +0300

    Done

---
+++ app.js
@@ -0,0 +1,25 @@
+// Dot env
+require("dotenv").config();
+
+// Express
+const express = require("express");
+const app = express();
+// Middleware Json
+app.use(express.json());
+// Port
+const port = process.env.PORT || 3000;
+// DB Connection
+const mongoose = require("mongoose");
+async function dbConnection() {
+    try {
+       await mongoose.connect(process.env.DB_URL);
+  console.log('Connected!')
+    } catch (error) {
+        console.log(error);
+    } 
+}
+dbConnection();
+// Listen / Run Server
+app.listen(port, () => {
+    console.log(`Server Is Running ${port}`);
+})
